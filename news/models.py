fb87ef290ae67fcb8654154c96148f64cb5da603
ARUNCO <duncanarani@gmail.com>
2018-11-01 14:17:16 +0300

    image upload

--- news/models.py
+++ news/models.py
@@ -33,7 +33,7 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_Image=models.imageField(upload_to='article/')
+    article_image=models.ImageField(upload_to='articles/',blank =True)
 
     def __str__(self):
         return self.title
